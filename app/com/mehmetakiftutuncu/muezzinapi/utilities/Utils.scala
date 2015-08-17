2c4548ed0fd84b35e21d0b81d4d742cb4a05052f
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2015-08-18 00:30:13 +0300

    Small but important changes and fixes, it is on Heroku now! :)

--- app/com/mehmetakiftutuncu/muezzinapi/utilities/Utils.scala
+++ app/com/mehmetakiftutuncu/muezzinapi/utilities/Utils.scala
@@ -15,29 +15,30 @@ object Utils {
    */
   def sanitizeHtml(str: String, replaceHtmlChars: Boolean = true, upperCaseEachWord: Boolean = true): String = {
     val result = {
-      if (replaceHtmlChars) {
-        str.trim.replaceAll("&#304;", "İ")
-          .replaceAll("&#214;", "Ö")
-          .replaceAll("&#220;", "Ü")
-          .replaceAll("&#199;", "Ç")
-          .replaceAll("&#286;", "Ğ")
-          .replaceAll("&#350;", "Ş")
-          .replaceAll("&#305;", "ı")
-          .replaceAll("&#246;", "ö")
-          .replaceAll("&#252;", "ü")
-          .replaceAll("&#231;", "ç")
-          .replaceAll("&#287;", "ğ")
-          .replaceAll("&#351;", "ş")
+      if (upperCaseEachWord) {
+        str.toLowerCase
+           .split(" ")
+           .map(w => w.take(1).toUpperCase + w.drop(1))
+           .mkString(" ")
+           .trim
       } else {
         str.trim
       }
     }
 
-    if (upperCaseEachWord) {
-      result.toLowerCase
-        .split(" ")
-        .map(w => w.take(1).toUpperCase + w.drop(1))
-        .mkString(" ")
+    if (replaceHtmlChars) {
+      result.replaceAll("&#304;", "İ")
+            .replaceAll("&#214;", "Ö")
+            .replaceAll("&#220;", "Ü")
+            .replaceAll("&#199;", "Ç")
+            .replaceAll("&#286;", "Ğ")
+            .replaceAll("&#350;", "Ş")
+            .replaceAll("&#305;", "ı")
+            .replaceAll("&#246;", "ö")
+            .replaceAll("&#252;", "ü")
+            .replaceAll("&#231;", "ç")
+            .replaceAll("&#287;", "ğ")
+            .replaceAll("&#351;", "ş")
     } else {
       result
     }
